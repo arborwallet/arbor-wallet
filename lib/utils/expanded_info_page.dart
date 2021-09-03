f493623bd632a1dc6ee9c90fb08038856649fd86
Alex G <hi@mavenblueprint.com>
2021-09-03 10:48:04 -0400

    Add all transactions button

--- lib/utils/expanded_info_page.dart
+++ lib/utils/expanded_info_page.dart
@@ -43,97 +43,105 @@ class _ExpandedInfoPageState extends State<ExpandedInfoPage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ValueListenableBuilder(
-          valueListenable: walletBox.listenable(),
-          builder: (context, Box box, widget) {
+        Expanded(
+          child: ValueListenableBuilder(
+            valueListenable: walletBox.listenable(),
+            builder: (context, Box box, widget) {
 
-            if (box.isEmpty) {
-              return const Center(
-                child: Text('Tap + to create a new wallet.'),
-              );
-            } else {
-              var walletData = walletBox.getAt(index);
-              return Column(
-                children: [
-                  ListTile(
-                    leading: Container(
-                      width: 50,
-                      height: 50,
-                      decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: new DecorationImage(
-                          image: AssetImage("assets/images/chia-logo.png"),
-                          fit: BoxFit.fitHeight,
+              if (box.isEmpty) {
+                return const Center(
+                  child: Text('Hmm. We could not find a wallet to show. Please try again'),
+                );
+              } else {
+                var walletData = walletBox.getAt(index);
+                return Column(
+                  children: [
+                    ListTile(
+                      leading: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                            image: AssetImage("assets/images/chia-logo.png"),
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                      ),
+                      title: Text('${walletData.fork.name} (${walletData.name})'),
+                      subtitle: Text(walletData.fork.ticker.toUpperCase()),
+                      trailing: IconButton(
+                        onPressed: () => _deleteInfo(index),
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
                         ),
                       ),
                     ),
-                    title: Text('${walletData.fork.name} (${walletData.name})'),
-                    subtitle: Text(walletData.fork.ticker.toUpperCase()),
-                    trailing: IconButton(
-                      onPressed: () => _deleteInfo(index),
-                      icon: const Icon(
-                        Icons.delete,
-                        color: Colors.red,
+                    Card(
+                      child: ListTile(
+                        title: FittedBox(fit: BoxFit.contain, child: Text('${walletData.fork.ticker.toUpperCase()}: ${walletData.balance.toStringAsFixed(walletData.fork.precision)}')),
+                        trailing: Icon(Icons.copy),
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(text: walletData.balance.toStringAsFixed(walletData.fork.precision)));
+                        },
                       ),
                     ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: FittedBox(fit: BoxFit.contain, child: Text('${walletData.fork.ticker.toUpperCase()}: ${walletData.balance.toStringAsFixed(walletData.fork.precision)}')),
-                      trailing: Icon(Icons.copy),
-                      onTap: () {
-                        Clipboard.setData(ClipboardData(text: walletData.balance.toStringAsFixed(walletData.fork.precision)));
-                      },
+                    Card(
+                      child: ListTile(
+                        title: Text('Address'),
+                        subtitle: Text(walletData.address),
+                        trailing: Icon(Icons.copy),
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(text: walletData.address));
+                        },
+                      ),
                     ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text('Address'),
-                      subtitle: Text(walletData.address),
-                      trailing: Icon(Icons.copy),
-                      onTap: () {
-                        Clipboard.setData(ClipboardData(text: walletData.address));
-                      },
+                    Card(
+                      child: ListTile(
+                        title: Text('Public Key'),
+                        subtitle: Text(walletData.publicKey),
+                        trailing: Icon(Icons.copy),
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(text: walletData.publicKey));
+                        },
+                      ),
                     ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text('Public Key'),
-                      subtitle: Text(walletData.publicKey),
-                      trailing: Icon(Icons.copy),
-                      onTap: () {
-                        Clipboard.setData(ClipboardData(text: walletData.publicKey));
-                      },
+                    Card(
+                      child: ListTile(
+                        title: Text('Private Key'),
+                        subtitle: Text('*' * walletData.privateKey.toString().length),
+                        trailing: Icon(Icons.copy),
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(text: walletData.privateKey));
+                        },
+                      ),
                     ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text('Private Key'),
-                      subtitle: Text('*' * walletData.privateKey.toString().length),
-                      trailing: Icon(Icons.copy),
-                      onTap: () {
-                        Clipboard.setData(ClipboardData(text: walletData.privateKey));
-                      },
-                    ),
-                  ),
-                ],
-              );
-            }
-          },
+                  ],
+                );
+              }
+            },
+          ),
         ),
-        const Spacer(),
+
+        OutlinedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(double.infinity, 30), // double.infinity is the width and 30 is the height
+            ),
+            onPressed: () {},
+            child: Text('All Transactions')
+        ),
         ListTile(
           contentPadding: EdgeInsets.all(10.0),//change for side padding
           title: Row(
             children: <Widget>[
-              Expanded(child: ElevatedButton(onPressed: () {},child: Text("Receive"))),
+              Expanded(child: ElevatedButton(onPressed: () {},child: Text('Receive'))),
               SizedBox(width: 10),
-              Expanded(child: ElevatedButton(onPressed: () {},child: Text("Send"))),
+              Expanded(child: ElevatedButton(onPressed: () {},child: Text('Send'))),
             ],
           ),
         )
-      ],
+      ]
     );
-
   }
 }
