d1d012577a27703d3897b39f9161ae59e9018ea9
oluwatobig <oluwatoyinoluwatobig@gmail.com>
2021-09-05 19:54:33 +0100

    center logo

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -19,7 +19,57 @@ class _SplashScreenState extends State<SplashScreen> {
       backgroundColor: Colors.green,
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 40),
-        child: Column(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                AssetPaths.logo,
+                width: MediaQuery.of(context).size.width * 0.5,
+                //height: MediaQuery.of(context).size.width * 0.5,
+              ),
+            ),
+            Positioned(
+              bottom: 40,right: 0,left: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Text(
+                    'Secure & Easy to Use Crypto Wallet',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  ArborButton(
+                    title: 'Get Started',
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute<Widget>(builder: (context)=>OnBoardingScreen()));
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ArborButton(
+                    backgroundColor: ArborColors.deepGreen,
+                    title: 'I already have a wallet',
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute<Widget>(builder: (context)=>InfoScreen()));
+                    },
+                  ),
+
+                ],
+              ),
+            ),
+
+          ],
+        ),
+        /*child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Image.asset(
@@ -62,8 +112,66 @@ class _SplashScreenState extends State<SplashScreen> {
               height: 40,
             ),
           ],
-        ),
+        ),*/
       ),
     );
   }
 }
+
+class NewSplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Center(
+            child: Image.asset(
+              AssetPaths.logo,
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.width * 0.5,
+            ),
+          ),
+          Positioned(
+            bottom: 40,right: 0,left: 0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text(
+                  'Secure & Easy to Use Crypto Wallet',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ArborButton(
+                  title: 'Get Started',
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute<Widget>(builder: (context)=>OnBoardingScreen()));
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ArborButton(
+                  backgroundColor: ArborColors.deepGreen,
+                  title: 'I already have a wallet',
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute<Widget>(builder: (context)=>InfoScreen()));
+                  },
+                ),
+
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
--- lib/views/widgets/layout/arbor_on_boarding_layout.dart
+++ lib/views/widgets/layout/arbor_on_boarding_layout.dart
@@ -349,7 +349,7 @@ class ArborOnBoardingLayoutState extends State<ArborOnBoardingLayout> {
             right: 0,
             child: Column(
               children: [
-                _toggleBtn(nextBtn, widget.showNextButton),
+                 _toggleBtn(nextBtn, widget.showNextButton),
                 const SizedBox(height: 20,),
                 Container(
                   padding: widget.controlsPadding,
