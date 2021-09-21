1c857e36bc0e52afc5a7ea6dc0ebb9922478943c
Oluwatobi Oluwatoyin <oluwatoyinoluwatobig@gmail.com>
2021-09-21 07:47:36 +0100

    create settings_screen

---
+++ lib/views/screens/settings_screen.dart
@@ -0,0 +1,73 @@
+import 'package:arbor/core/constants/asset_paths.dart';
+import '../../core/constants/arbor_colors.dart';
+import 'package:flutter/material.dart';
+
+class RestoreWalletScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: ArborColors.green,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: ArborColors.green,
+          appBar: AppBar(
+            backgroundColor: ArborColors.green,
+            centerTitle: true,
+              title: Text(
+                'Settings',
+                style: TextStyle(
+                  color: ArborColors.white,
+                ),
+              ),
+              leading:  IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: ArborColors.white,
+                ),
+              )
+          ),
+          body: Container(
+            child: ListView(
+              padding: EdgeInsets.all(16),
+              children: [
+                Text("General"),
+                settingsItem("Visit DFI Discord Channel"),
+                settingsItem("View Privacy Policy"),
+                SizedBox(height:10),
+                Text("Arbor Data"),
+                settingsItem("Delete Arbor Data"),
+                
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  
+  Widget settingsItem(String title){
+    return Container(
+                  padding:EdgeInsets.all(10,),
+                  margin:EdgeInsets.only(bottom:10),
+                  decoration:BoxDecoration(
+                    color: ArborColors.deepGreen,
+                    radius:BorderRadius.all(Radius.circular(8),),
+                  )
+                  child:Row(
+                    mainAxisAlignment: MainAxisAlignment.start;
+                    crossAxisAlignment: CrossAxisAlignment.center;
+                    children:[
+                  
+                      SizedBox(width:16),
+                      Text("Visit DFI Discord Channel"),
+                      
+                    ]
+                  )
+                );
+  }
+  
+}
