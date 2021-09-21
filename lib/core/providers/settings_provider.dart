ceea23d84085698304da030fd2718845ef8e1396
Oluwatobi Oluwatoyin <oluwatoyinoluwatobig@gmail.com>
2021-09-21 07:52:18 +0100

    Update settings_provider.dart

--- lib/core/providers/settings_provider.dart
+++ lib/core/providers/settings_provider.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/foundation.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 class SettingsProvider extends ChangeNotifier {
+  
+  
+  launchURL({required String url}) async {
+    await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
+  }
 
 
 }
