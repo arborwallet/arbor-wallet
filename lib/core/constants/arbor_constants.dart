b481560cea167055d2c648853c3b7666bef26002
Alex G <hi+github@mavenblueprint.com>
2021-09-28 11:07:01 -0400

    Fix discord invite link

--- lib/core/constants/arbor_constants.dart
+++ lib/core/constants/arbor_constants.dart
@@ -1,6 +1,6 @@
 abstract class ArborConstants {
   static const String baseWebsiteURL = 'https://www.arborwallet.com';
-  static const String discordChannelURL = 'https://discord.gg/GESDCaUn';
+  static const String discordChannelURL = 'https://discord.gg/WT4qxy2JBV';
   static const String explorerBaseURL="https://www.chiaexplorer.com/blockchain/address";
   static final String IS_FIRST_TIME_USER_KEY = 'is_first_time_user_key';
   static const String websitePrivacyURL = '${baseWebsiteURL}/privacy/en';
