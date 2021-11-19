eccab21f91379af237539329baa4c2e8bfc70c5d
Alex G <hi+github@mavenblueprint.com>
2021-11-18 23:09:45 -0500

    Remove aggSigMe since it's retreived from API

--- lib/core/utils/wallet_utils.dart
+++ lib/core/utils/wallet_utils.dart
@@ -6,9 +6,6 @@ import 'package:arbor/clvm/program.dart';
 final walletPuzzle = Program.parse(
     '(a (q 4 (c 4 (c 5 (c (a 6 (c 2 (c 11 ()))) ()))) 11) (c (q 50 2 (i (l 5) (q 11 (q . 2) (a 6 (c 2 (c 9 ()))) (a 6 (c 2 (c 13 ())))) (q 11 (q . 1) 5)) 1) 1))');
 
-const aggSigMeExtraData =
-    'ccd5bb71183532bff220ba46c268991a3ff07eb358e8255a65c30a2dce0e5fbb';
-
 List<RecordResponse> aggregateRecords(
     List<RecordResponse> records, int totalAmount) {
   List<RecordResponse> spendRecords = [];
